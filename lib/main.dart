a54da3621cc07a290642904a838b2c2a59cfaafc
SAFI174 <moustafa.masri.1522@gmail.com>
2023-04-01 01:18:22 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import 'app/routes/app_pages.dart';
 
